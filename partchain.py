3df36ed93b06d75def1f85ab1d68f4273b16c355
Alessandro Surace <3593840+zioalex@users.noreply.github.com>
2018-10-28 11:52:44 +0100

    Presentation preparation

--- partchain.py
+++ partchain.py
@@ -1,50 +1,85 @@
-#!/home/asurace/VirtualEnvs/blockchain/bin/python3.6
 """
+                                                                                                                                             
+8 888888888o      .8.          8 888888888o. 8888888 8888888888 ,o888888o.    8 8888        8          .8.           8 8888 b.             8 
+8 8888    `88.   .888.         8 8888    `88.      8 8888      8888     `88.  8 8888        8         .888.          8 8888 888o.          8 
+8 8888     `88  :88888.        8 8888     `88      8 8888   ,8 8888       `8. 8 8888        8        :88888.         8 8888 Y88888o.       8 
+8 8888     ,88 . `88888.       8 8888     ,88      8 8888   88 8888           8 8888        8       . `88888.        8 8888 .`Y888888o.    8 
+8 8888.   ,88'.8. `88888.      8 8888.   ,88'      8 8888   88 8888           8 8888        8      .8. `88888.       8 8888 8o. `Y888888o. 8 
+8 888888888P'.8`8. `88888.     8 888888888P'       8 8888   88 8888           8 8888        8     .8`8. `88888.      8 8888 8`Y8o. `Y88888o8 
+8 8888      .8' `8. `88888.    8 8888`8b           8 8888   88 8888           8 8888888888888    .8' `8. `88888.     8 8888 8   `Y8o. `Y8888 
+8 8888     .8'   `8. `88888.   8 8888 `8b.         8 8888   `8 8888       .8' 8 8888        8   .8'   `8. `88888.    8 8888 8      `Y8o. `Y8 
+8 8888    .888888888. `88888.  8 8888   `8b.       8 8888      8888     ,88'  8 8888        8  .888888888. `88888.   8 8888 8         `Y8o.` 
+8 8888   .8'       `8. `88888. 8 8888     `88.     8 8888       `8888888P'    8 8888        8 .8'       `8. `88888.  8 8888 8            `Yo 
+
 Testing:
 
-neo> build 4-domain.py test 0710 05 True False query ["test.com"]
-neo> build 4-domain.py test 0710 05 True False register ["test.com","AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y"]
-neo> build 4-domain.py test 0710 05 True False delete ["test.com"]
-neo> build 4-domain.py test 0710 05 True False transfer ["test.com","AK2nJJpJr6o664CWJKi1QRXjqeic"]
-
-build 4-domain.py test 0710 05 True False False register ["test.com","AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y"]
-build partchain.py test 0710 05 True False False registerpart ["1234567890","Radar","Wavelength: 10.71 cm , Peak transmit power: 500 kW (250 kW horizontal, 250 kW vertical) , Pulse duration: 0.72 microseconds , Minimum detectable signal: -108 dBm", "2018-10-27", "10 years", "New", "AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y"]
+neo> build partchain.py test 0710 05 True False False registerpart ["1234567890","Radar","Wavelength: 10.71 cm , Peak transmit power: 500 kW (250 kW horizontal, 250 kW vertical) , Pulse duration: 0.72 microseconds , Minimum detectable signal: -108 dBm", "2018-10-27", "10 years", "New", "AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y"]
 
 RegisterPart(serial_number, part_name, tech_spec, manufacture_date, lifespan, current_status, owner)
 
 Importing:
 
-neo> import contract 4-domain.avm 0710 05 True False False
-neo> contract search ...
-
-import contract partchain.avm  0710 05 True False False
+> import contract partchain.avm  0710 05 True False False
 
 Using:
+neo> testinvoke 0x2df618fb7d04f69a4d3f8c9da33982fbb5c80e4a  registerpart ["1234567890","Radar","Wavelength: 10.71 cm , Peak transmit power: 500 kW (250 kW horizontal, 250 kW vertical) , Pulse duration: 0.72 microseconds , Minimum detectable signal: -108 dBm", "2018-10-27", "10 years", "New", "AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y"]
 
-neo> testinvoke 5030694901a527908ab0a1494670109e7b85e3e4 query ["test.com"]
-neo> testinvoke 5030694901a527908ab0a1494670109e7b85e3e4 register ["test.com","AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y"]
-neo> testinvoke 5030694901a527908ab0a1494670109e7b85e3e4 delete ["test.com"]
-neo> testinvoke 5030694901a527908ab0a1494670109e7b85e3e4 transfer ["test.com","AZ9Bmz6qmboZ4ry1z8p2KF3ftyA2ckJAym"]
 """
-from boa.interop.Neo.Runtime import Log, Notify
-from boa.interop.Neo.Storage import Get, Put, GetContext
-from boa.interop.Neo.Runtime import GetTrigger,CheckWitness
 from boa.builtins import concat
+from boa.interop.Neo.Action import RegisterAction
+from boa.interop.Neo.App import DynamicAppCall
+from boa.interop.Neo.Blockchain import GetContract
+from boa.interop.Neo.Iterator import Iterator
+from boa.interop.Neo.Runtime import (CheckWitness, GetTrigger, Log,
+                                     Notify, Serialize)
+from boa.interop.Neo.Storage import GetContext, Get, Put, Delete, Find
+from boa.interop.Neo.TriggerType import Application, Verification
+from boa.interop.System.ExecutionEngine import (GetCallingScriptHash,
+                                                GetEntryScriptHash,
+                                                GetExecutingScriptHash)
 
 
 
 ## datastructure
-
+#  _   _           _       ______                           _   _       _          
+# | \ | |         | |     |  ____|                         (_) | |     | |         
+# |  \| |   ___   | |_    | |__     _   _   _ __     __ _   _  | |__   | |   ___   
+# | . ` |  / _ \  | __|   |  __|   | | | | | '_ \   / _` | | | | '_ \  | |  / _ \  
+# | |\  | | (_) | | |_    | |      | |_| | | | | | | (_| | | | | |_) | | | |  __/  
+# |_| \_|  \___/   \__|   |_|       \__,_| |_| |_|  \__, | |_| |_.__/  |_|  \___|  
+#                                                    __/ |                         
+#  _______           _                              |___/   _   ______   _______   
+# |__   __|         | |                                | \ | | |  ____| |__   __|  
+#    | |      ___   | | __   ___   _ __      ______    |  \| | | |__       | |     
+#    | |     / _ \  | |/ /  / _ \ | '_ \    |______|   | . ` | |  __|      | |     
+#    | |    | (_) | |   <  |  __/ | | | |              | |\  | | |         | |     
+#    |_|     \___/  |_|\_\  \___| |_| |_|              |_| \_| |_|         |_|     
+#                                                                                  
+                                                                                  
 manufacture_data_json = {   "Serial_number": "", 
                             "Part_name": "",
                             "TechSpec": "",
                             "Certified_by": "",
                             "Manufacture_date": "",
                             "Lifespan": "", # Hours of flight, Km, 
+                            "blockchain_address": "" # This will contain the Fungible Token adress
+                        }
+
+# The follow data will be added in the Fungible Token 
+#  ______                           _   _       _            _______           _                   
+# |  ____|                         (_) | |     | |          |__   __|         | |                  
+# | |__     _   _   _ __     __ _   _  | |__   | |   ___       | |      ___   | | __   ___   _ __  
+# |  __|   | | | | | '_ \   / _` | | | | '_ \  | |  / _ \      | |     / _ \  | |/ /  / _ \ | '_ \ 
+# | |      | |_| | | | | | | (_| | | | | |_) | | | |  __/      | |    | (_) | |   <  |  __/ | | | |
+# |_|       \__,_| |_| |_|  \__, | |_| |_.__/  |_|  \___|      |_|     \___/  |_|\_\  \___| |_| |_|
+#                            __/ |                                                                 
+#                           |___/                                                                 
+
+history_data_json = {
                             "Current_status": "New / Certified / Uncertified / Implemented / Refurbished / Out of order", 
                             "Repair_history": { "Repair_company": "", "Status": "Ok/Failure", "ID_technician": "", "Current_usage": "Km, hours" },
                             "Ownerhip_history": { Owner: "Boing", Aircraft_ID: "2312321386213" }
-                        }
+                    }
 
 
 def Main(operation, args):
